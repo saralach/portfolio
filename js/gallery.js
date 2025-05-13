f0ec897a383558bf14f2208af473529a8eb901f8
saralach <slach11303@gmail.com>
2025-05-13 12:54:41 -0500

    Added gallery modal upon clicking on an image

--- js/gallery.js
+++ js/gallery.js
@@ -1,22 +1,47 @@
 let currentIndex = 0;
-
-//const slides = document.querySelectorAll('.slide');
-const slides = document.querySelectorAll('.slide');
+const slides = document.querySelectorAll("#gallery figure");
 const totalSlides = slides.length;
 
-console.log(slides);
+const modal = document.getElementById("galleryModal");
+const modalImg = document.getElementById("modalImg");
+const closeModalBtn = document.getElementById("closeBtn");
 
-// ----------- Event Listeners -----------
-document.querySelector('#next').addEventListener('click', () => {
+// =============== Slide Arrows Functionality ===============
+document.querySelector("#next").addEventListener("click", () => {
   changeSlide(currentIndex + 1);
 });
 
-document.querySelector('#previous').addEventListener('click', () => {
+document.querySelector("#previous").addEventListener("click", () => {
   changeSlide(currentIndex - 1);
 });
 
 function changeSlide(index) {
-  slides[currentIndex].classList.remove('active');
+  slides[currentIndex].classList.remove("active");
   currentIndex = (index + totalSlides) % totalSlides;
-  slides[currentIndex].classList.add('active');
+  slides[currentIndex].classList.add("active");
 }
+
+
+// ================== Modal Functionality ===================
+// Enable clicking on an image to open modal
+slides.forEach(slide => {
+  slide.addEventListener("click", () => {
+    const slideImg = slide.querySelector("img");
+    modal.style.display = "block";
+    modalImg.src = slideImg.src;
+  });
+});
+
+// Closing modal with X button
+slides.forEach(slide => {
+  closeModalBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+});
+
+// Closing modal by clicking outside of the modal
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
