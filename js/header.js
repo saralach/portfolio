3818ccb87da321a97045b7285319b1de2b6d723c
saralach <slach11303@gmail.com>
2025-05-15 12:23:28 -0500

    Updated header display

--- js/header.js
+++ js/header.js
@@ -1,3 +1,10 @@
-fetch('../includes/header.html').then(res => res.text()).then(data => {
-    document.getElementById('header').innerHTML = data;
-  });
+const pathParts = window.location.pathname.split('/');
+let path = "/includes/header.html";
+
+// If on "portfolio" path (on GitHub Pages), make that the base of the path
+if(pathParts[0] === "portfolio")
+  path += "portfolio";
+
+fetch(path).then(res => res.text()).then(data => {
+  document.getElementById('header').innerHTML = data;
+});
