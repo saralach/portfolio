c189c88c6a36f1fc2d54525c7cae72d24be9bca5
saralach <slach11303@gmail.com>
2025-05-20 14:45:55 -0500

    Tweaked WILD email file path

--- js/wild-email.js
+++ js/wild-email.js
@@ -1,3 +1,6 @@
-fetch('../images/WILD/WILD-Email-Design.html').then(res => res.text()).then(data => {
+const hostNm = window.location.hostname;
+const root = hostName.includes("github") ? "/portfolio": "";
+
+fetch(`${root}/images/WILD/WILD-Email-Design.html`).then(res => res.text()).then(data => {
   document.getElementById('emailContent').innerHTML = data;
 });
